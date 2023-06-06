c857c8148abf347f705ca3de3f9254277f1b8a6c
l11EG4 <zakariaberahmi@gmail.com>
2023-06-06 07:08:23 -0700

    Mega

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for s in reversed(range(97, 123)):
-    print("{:c}.format(s if (s % 2 == 0) else (s - 32)), end='')
+    print("{:c}".format(s if (s % 2 == 0) else sh - 32)), end='')
