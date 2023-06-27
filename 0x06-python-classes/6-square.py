558f7f4eee8a6cf05c8192c0e269d0ed2bbad251
l11EG4 <zakariaberahmi@gmail.com>
2023-06-27 01:34:54 -0700

    Mega

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python3
+
+# Made by Megatron
+
+"""6 Coordinates of a square"""
+
+
+class Square:
+    """This is class"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.__size
+
+    @property
+    def position(self):
+        """Getter 4 position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Setter 4 position"""
+        if type(value) is not tuple or len(value) != 2 or \
+                type(value[0]) is not int or type(value[1]) is not int or \
+                value[0] < 0 or value[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    @size.setter
+    def size(self, value):
+        """Setter 4 size"""
+        if type(value) is not int:
+            raise TypeError("the size must be an integer")
+        if value < 0:
+            raise ValueError("the size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current squar"""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Print squar using # taking into account position"""
+        if not self.size:
+            print()
+            return
+
+        print("\n" * self.position[1], end="")
+        print("\n".join([" " * self.position[0] +
+                        "#" * self.size
+                         for i in range(self.size)]))
