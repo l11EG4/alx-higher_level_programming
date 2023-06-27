b3ad9a759f8cc6b9080baa9d98bfde0d41c750c8
l11EG4 <zakariaberahmi@gmail.com>
2023-06-27 01:28:48 -0700

    Mega

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+# Made by MEGATRON
+
+"""Define a class Squar"""
+
+
+class Square:
+    """Represent a squar"""
+
+    def __init__(self, size):
+        """Init new squar
+
+        Args:
+            size (int): The size of the new squar
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Get/set the current size of the squar"""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("the size must be an integer")
+        elif value < 0:
+            raise ValueError("the size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return current area of the squar"""
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Print squar with the # char"""
+        for i in range(0, self.__size):
+            [print("#", end="") for j in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
