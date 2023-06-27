4e5c529b4f20de39286757c5715563d135ab003c
l11EG4 <zakariaberahmi@gmail.com>
2023-06-27 02:08:11 -0700

    Mega

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -15,9 +15,9 @@ class Square:
             size (int): size of the new squar
         """
         if not isinstance(size, int):
-            raise TypeError("the size must be an integer")
+            raise TypeError("size must be an integer")
         elif size < 0:
-            raise ValueError("the size must be >= 0")
+            raise ValueError("size must be >= 0")
         self.__size = size
 
     def area(self):
