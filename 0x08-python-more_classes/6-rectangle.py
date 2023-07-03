1e74644bf4ebb391d9c0dd646ea1b071cd669638
l11EG4 <zakariaberahmi@gmail.com>
2023-07-03 12:58:54 -0700

    Mega

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-Module 2-rectangle
-class Rectangle
+Module 6-rectangle
+made by mega
 """
 
 
-class Rectangle:
+class Rectangle():
     """
     Defines class rectangle with private attribute width and height
 
@@ -13,17 +13,33 @@ class Rectangle:
         width (int): width
         height (int): height
 
+    Attributes:
+        number_of_instances (int): number of instances created and not deleted
+
     Functions:
         __init__(self, width, height)
         width(self)
         width(self, value)
         height(self)
         height(self, value)
+        area(self)
+        perimeter(self)
+        __str__(self)
+        __repr__(self)
+        __del__(self)
     """
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """ Initialize rectangles """
         self.width = width
         self.height = height
+        type(self).number_of_instances += 1
+
+    def __del__(self):
+        """ Deletes instance of class """
+        print("Bye rectangle...")
+        type(self).number_of_instances -= 1
 
     @property
     def width(self):
@@ -43,12 +59,3 @@ class Rectangle:
     def height(self):
         """ Getter returns height """
         return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ Setter sets height if int > 0 """
-        if not isinstance(value, int):
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
