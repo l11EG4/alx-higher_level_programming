c4c9dc5c07015e6aa8c298568d0843bc8035edac
l11EG4 <zakariaberahmi@gmail.com>
2023-07-04 04:18:50 -0700

    Mega

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
 
+# Made by MEGATRON
+
 """Defines a locked class"""
 
 
 class LockedClass:
+    """
+    Prevent user from instantiating new LockedClass attributes
+    for anything but attributes called 'first_name'.
+    """
 
     __slots__ = ["first_name"]
