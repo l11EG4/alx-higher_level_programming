b086266dff6ad82af7ff212ac920cb2605e8b659
l11EG4 <zakariaberahmi@gmail.com>
2023-07-04 03:03:02 -0700

    Mega

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-
-# Made By MEGATRON
-
-"""Module to find the max intg in a list"""
+"""Module to find the max integer in a list
+"""
 
 
 def max_integer(list=[]):
-    """Funct to find & return the max intg in a list of intgs"""
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
+    """
     if len(list) == 0:
         return None
     result = list[0]
