3f8c0b3612c8c811592304569c3820713edd5c24
l11EG4 <zakariaberahmi@gmail.com>
2023-07-03 17:54:08 -0700

    Mega

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -6,7 +6,7 @@ import sys
 
 
 def init_board(n):
-    """Initialize an `n`x`n` sized chessboard with 0's."""
+    """Initialize an n x n sized chessboard with zero's"""
     board = []
     [board.append([]) for i in range(n)]
     [row.append(' ') for i in range(n) for row in board]
@@ -14,14 +14,14 @@ def init_board(n):
 
 
 def board_deepcopy(board):
-    """Return a deepcopy of a chessboard."""
+    """Return deepcopy of a chess board"""
     if isinstance(board, list):
         return list(map(board_deepcopy, board))
     return (board)
 
 
 def get_solution(board):
-    """Return the list of lists representation of a solved chessboard."""
+    """Return the list of lists representation of a solved chess board"""
     solution = []
     for r in range(len(board)):
         for c in range(len(board)):
