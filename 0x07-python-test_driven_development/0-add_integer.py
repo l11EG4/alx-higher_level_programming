c812930effb6f7344586f4dd085e5af76ce322d9
l11EG4 <zakariaberahmi@gmail.com>
2023-07-04 02:36:16 -0700

    Mega

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 0-add_integer.py
+
+# Made By MEGATRON
+
+"""Defines intg addit funct"""
+
+
+def add_integer(a, b=98):
+    """Return the intg"""
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
