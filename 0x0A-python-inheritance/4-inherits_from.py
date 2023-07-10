3f7fc629589f830a91e29a657f780675a1247b40
l11EG4 <zakariaberahmi@gmail.com>
2023-07-10 10:53:40 -0700

    Mega

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 Module 4-inherits_from.py
 
 Contains the method inherits_from
-returns True if object is instance of class that it inherits from or is subcls of
+returns True if obj is instance of class that it inherits from or is subcls of
 """
 
 
@@ -16,6 +16,6 @@ def inherits_from(obj, a_class):
         use issubclass() to get what object is a subclass of
 
     Return:
-        True if object is instance of class that it inherits from or is subcls of
+        True if obj is instance of class that it inherits from or is subcls of
     """
     return (type(obj) is not a_class and issubclass(type(obj), a_class))
