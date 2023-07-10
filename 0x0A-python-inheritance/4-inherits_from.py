535e576bb2d2f353ad5af8d4f6616d4b8db22089
l11EG4 <zakariaberahmi@gmail.com>
2023-07-10 10:52:12 -0700

    Mega

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# Made by Megatron
+"""
+Module 4-inherits_from.py
+
+Contains the method inherits_from
+returns True if object is instance of class that it inherits from or is subcls of
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Notes:
+        use type() to get specific class
+        use isinstance() to get class and any parent classes too
+        use issubclass() to get what object is a subclass of
+
+    Return:
+        True if object is instance of class that it inherits from or is subcls of
+    """
+    return (type(obj) is not a_class and issubclass(type(obj), a_class))
