b8daa683337d4b32c9bf7af8f153ca3e7bf209af
l11EG4 <zakariaberahmi@gmail.com>
2023-07-10 10:57:09 -0700

    Mega

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+# Made By MEGATRON
+"""
+Module 6-base_geometry
+
+Contains empty class BaseGeometry with pub instance method area
+"""
+
+
+class BaseGeometry:
+    """
+    Methods:
+        area(self)
+    """
+    def area(self):
+        """not implemented"""
+        raise Exception("area() is not implemented")
