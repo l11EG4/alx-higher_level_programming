b8824bea9b7177544b660a071c973f5cc98cdcd9
l11EG4 <zakariaberahmi@gmail.com>
2023-07-10 10:28:50 -0700

    Mega

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Made by Megatron
+"""
+Module 0-lookup
+
+Contains a method lookup that returns list of objects attribute and methods
+"""
+
+
+def lookup(obj):
+    """returns a list of object's attribute and methods"""
+    return dir(obj)
