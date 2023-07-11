a41696528e8a08d7a1c085c9c4fad1576ce064dd
l11EG4 <zakariaberahmi@gmail.com>
2023-07-11 05:22:52 -0700

    Mega

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# Made by MEGATRON
+"""
+adds and saves to Python object to JSON file; loads objects
+"""
+
+
+from sys import argv
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    existing_content = load_from_json_file(filename)
+except FileNotFoundError:
+    existing_content = []
+
+save_to_json_file(existing_content + argv[1:], filename)
