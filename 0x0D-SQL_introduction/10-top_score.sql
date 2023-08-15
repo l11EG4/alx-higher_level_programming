f712d5dc47e64dbf4893f4a9077a300adf28d99c
l11EG4 <zakariaberahmi@gmail.com>
2023-08-15 04:35:12 -0700

    Mega

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,10 @@
+-- display score and name fields of data in table
+-- ordered from greatest to least
+-- Made by MEGATRON
+-- score   name
+-- 14  Bob
+-- 10  John
+-- 8   George
+-- 3   Alex
+
+SELECT score, name FROM second_table ORDER BY score DESC;
