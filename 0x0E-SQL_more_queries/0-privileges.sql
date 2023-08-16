8298651160595e9c78a14476a9d7a157e4e597fa
l11EG4 <zakariaberahmi@gmail.com>
2023-08-16 04:12:31 -0700

    Mega

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,6 +1,5 @@
 -- script that lists all privileges of the MySQL
--- users user_0d_1 and user_0d_2 on local server
--- Made by MEGATRON
+-- users user_0d_1 and user_0d_2 on local serve
 
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
