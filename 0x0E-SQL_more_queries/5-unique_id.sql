d86502c9074837c7ee845952686841712b09d33b
l11EG4 <zakariaberahmi@gmail.com>
2023-08-16 03:50:07 -0700

    Mega

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,9 @@
+-- creates the table unique_id
+-- description of table data: id INT with the default value 1, must be unique & name VARCHAR(256)
+-- should not fail if exists
+-- Made by MEGATRON
+
+CREATE TABLE IF NOT EXISTS unique_id (
+       id INT DEFAULT 1 UNIQUE,
+       name VARCHAR(256)
+);
