d5cb71d0ed655df3c9f600abb517c3ab8e05f480
l11EG4 <zakariaberahmi@gmail.com>
2023-09-29 08:19:20 -0700

    Mega

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Made by MEGA
+"""A  script that:
+- takes in a URL and an email address
+- sends a POST request to the passed URL with the email as a parameter
+- displays the body of the response.
+"""
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    url = argv[1]
+    payload = {'email': argv[2]}
+    r = requests.post(url, data=payload)
+    print(r.text)
